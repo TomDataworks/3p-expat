57c66366099cfef274892ba31366e2f1a40893a0
Drake Arconis <drake@alchemyviewer.org>
2015-07-21 09:48:41 -0400

    Switch to building VS14 libraries

--- build-cmd.sh
+++ build-cmd.sh
@@ -33,10 +33,10 @@ pushd "$top/$EXPAT_SOURCE_DIR"
             load_vsvars
             set -x
 
-            cmake -G"Visual Studio 12" . -DBUILD_shared:BOOL=OFF
+            cmake -G"Visual Studio 14" . -DBUILD_shared:BOOL=OFF
 
-            build_sln "expat.sln" "Debug|Win32" "expat" || exit 1
-            build_sln "expat.sln" "Release|Win32"  "expat" || exit 1
+            build_sln "expat.sln" "Debug|Win32" "expat"
+            build_sln "expat.sln" "Release|Win32"  "expat"
 
             mkdir -p "$stage/lib/debug"
             mkdir -p "$stage/lib/release"
@@ -53,10 +53,10 @@ pushd "$top/$EXPAT_SOURCE_DIR"
             load_vsvars
             set -x
 
-            cmake -G"Visual Studio 12 Win64" . -DBUILD_shared:BOOL=OFF
+            cmake -G"Visual Studio 14 Win64" . -DBUILD_shared:BOOL=OFF
 
-            build_sln "expat.sln" "Debug|x64" "expat" || exit 1
-            build_sln "expat.sln" "Release|x64"  "expat" || exit 1
+            build_sln "expat.sln" "Debug|x64" "expat"
+            build_sln "expat.sln" "Release|x64"  "expat"
 
             mkdir -p "$stage/lib/debug"
             mkdir -p "$stage/lib/release"
